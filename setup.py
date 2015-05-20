2b0cd5eb785c702e33853746e5affe4591b38095
Tycho Bismeijer <t.bismeijer@nki.nl>
2015-05-20 14:27:52 +0200

    Use numpy blas/lapack

--- setup.py
+++ setup.py
@@ -2,14 +2,19 @@ from setuptools import setup
 from setuptools.extension import Extension
 from Cython.Build import cythonize
 
+from numpy.distutils.system_info import get_info
+
 setup(
     name='sfa',
     version='0.1-dev',
     author='Tycho Bismeijer',
     author_email='t.bismeijer@nki.nl',
     packages=['sfa'],
-    ext_modules=cythonize([Extension('sfa._sfa',
-                                     ['sfa/_sfa.pyx', 'sfa-c/src/sfa.c'],
-                                     libraries=['blas', 'lapacke'],
-                                     include_dirs=['sfa-c/include'])]),
+    ext_modules=cythonize([Extension(
+        'sfa._sfa',
+        ['sfa/_sfa.pyx', 'sfa-c/src/sfa.c'],
+        libraries=get_info('blas_opt')['libraries'] +
+                  get_info('lapack_opt')['libraries'] +
+                  ['lapacke', 'm'],
+        include_dirs=['sfa-c/include'])]),
 )
